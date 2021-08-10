c67d2f0944695df51378e1793e5a485842691d0e
Jeeshan <jeeshan.ahmed@soprasteria.com>
2021-08-10 22:12:29 +0530

    book store

---
+++ src/FirstComponent.js
@@ -0,0 +1,11 @@
+import React
+import {render } from "react-dom";
+function FirdstComponent() {
+    return (
+        <React.Fragment>
+            <div>
+                <h2>This is my first component</h2>
+            </div>
+        </React.Fragment>
+    )
+}
